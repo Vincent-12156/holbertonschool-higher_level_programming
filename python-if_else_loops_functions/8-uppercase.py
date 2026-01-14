5f33a513c6409c51dcb4f49cf720b9647539835c
Vincent-12156 <12156@holbertonstudents.com>
2026-01-14 14:24:05 +0100

    Change if condition in 8-file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     result = ""
     for c in str:
-        if 97 <= ord(c) <= 122:
-            result += chr(ord(char) - 32)
+        if ord(c) >= 97 and ord(c) <= 122:
+            result += chr(ord(c) - 32)
         else:
-            result += char
+            result += c
     print("{}".format(result))
