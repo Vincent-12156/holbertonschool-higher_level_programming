7a98cda687366addbb3b765fbb10f5db81c1537f
Vincent-12156 <12156@holbertonstudents.com>
2026-01-21 11:51:51 +0100

    Pycodestyle

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         quotien = None
     finally:
-            print("Inside result: {}".format(quotien))
+        print("Inside result: {}".format(quotien))
     return quotien
