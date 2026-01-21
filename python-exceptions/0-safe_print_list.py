bf86a8511c1b87c140e03b19e65e70ae4793d0c5
Vincent-12156 <12156@holbertonstudents.com>
2026-01-21 10:38:21 +0100

    Add T.3.0 file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            nb += 1
+    except IndexError:
+        pass
+    print()
+    return nb
