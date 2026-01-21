8db3c8634f583a4b2cc0fff3ef2de7ca020464a6
Vincent-12156 <12156@holbertonstudents.com>
2026-01-21 10:02:38 +0100

    Add check for len=0 in 6-file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
+            if j != 0:
+                print(" ", end="")
             print("{:d}".format(i[j]), end=" ")
         print()
