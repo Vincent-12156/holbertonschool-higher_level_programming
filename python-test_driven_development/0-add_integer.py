19d298b5de474e4c6d1b823f8dcaf280e6ba37e8
Vincent-12156 <12156@holbertonstudents.com>
2026-01-22 15:17:01 +0100

    Change 4.0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,8 +9,6 @@ their types.
 def add_integer(a, b=98):
     """
     Adds two integers.
-    a (int or float): first number
-    b (int or float): second number, defaults to 98
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
