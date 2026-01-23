8bf1f27481073bb00df969648cce35c2100ba6f5
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 14:15:03 +0100

    change the way to 2 new lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -13,13 +13,14 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    if text == "":
+        raise ValueError("text must be a string")
     i = 0
     while i < len(text):
         char = text[i]
         print(char, end="")
         if char in ['.', '?', ':']:
-            print()
-            print()
+            print("\n")
             if i + 1 < len(text) and text[i + 1] == " ":
                 i += 1
         i += 1
