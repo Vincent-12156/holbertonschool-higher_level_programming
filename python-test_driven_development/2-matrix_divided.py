f3e877893f3e82cce6e966167e2d68263e68b510
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 11:08:51 +0100

    Change new_matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Docstring for python-test_driven_development.2-matrix_divided
+Module for dividing all elements of a matrix by a number.
 matrix: list of lists of integers or floats
 div: number (integer or float)
 """
@@ -8,7 +8,7 @@ div: number (integer or float)
 
 def matrix_divided(matrix, div):
     """
-    Docstring for matrix_divided
+    Divides all elements of a matrix by div, rounded to 2 decimals.
     """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
@@ -17,14 +17,22 @@ def matrix_divided(matrix, div):
     if matrix == []:
         raise ValueError(
             "matrix must be a matrix (list of lists) of integers/floats")
-    row_length = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
-        for i in row:
-            if not isinstance(i, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) "
-                    "of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    return [[round(i / div, 2) for i in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        if row == matrix[0]:
+            len_1 = len(row)
+        len_2 = len(row)
+        if len_1 != len_2:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for j in range(len(row)):
+            if isinstance(row[j], (int, float)):
+                new_row.append(round(row[j] / div, 2))
+            else:
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+        new_matrix.append(new_row)
+    return new_matrix
