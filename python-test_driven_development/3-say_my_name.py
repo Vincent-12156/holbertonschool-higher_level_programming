cb5f4d4b54424be9c4abb3ee5b35063bec30330b
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 12:25:39 +0100

    handle empty string

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name=""):
     """
     if first_name == "":
         raise ValueError("first_name must be a string")
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) and not "":
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
