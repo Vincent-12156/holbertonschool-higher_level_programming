0494bcd0cdcd46fb809c0dfdfda73f039dd9f4c2
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 14:04:46 +0100

    Add 4.5 files

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+text must be a string,
+otherwise raise a TypeError exception with
+the message text must be a string
+"""
+
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines
+    after each of these characters: ., ? and
+    """
+    if text == "":
+        raise ValueError("text must be a string")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        char = text[i]
+        print(char, end="")
+        if char in ['.', '?', ':']:
+            print("\n")
+            if i + 1 < len(text) and text[i + 1] == " ":
+                i += 1
+        i += 1
