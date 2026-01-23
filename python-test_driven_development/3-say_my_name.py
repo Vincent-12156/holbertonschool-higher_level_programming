77e1da7efac515e16214af97b741d26314f822c3
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 11:39:10 +0100

    Add 4.3 files

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+first_name and last_name must be strings otherwise,
+raise a TypeError exception with the message first_name
+must be a string or last_name must be a string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is first name and last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    name = first_name
+    if last_name != "":
+        name += " " + last_name
+    print("My name is",name)
+
