706caf1a70c8ee32a459d730cfcf9476aa016826
Vincent-12156 <12156@holbertonstudents.com>
2026-01-23 10:48:23 +0100

    Change for value error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -17,7 +17,7 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or
             matrix == [] or
             not all(isinstance(row, list) for row in matrix)):
-        raise TypeError(
+        raise ValueError(
             "matrix must be a matrix (list of lists) of integers/floats")
     row_length = len(matrix[0])
     for row in matrix:
